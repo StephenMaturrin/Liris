299fffef92c8bba35f7273d448a2ce0979222ea0
Maturrin <migue.solinas@gmail.com>
2017-11-25 13:18:31 +0100

    1

--- Import.py
+++ Import.py
@@ -1,1 +1,16 @@
-print("hello")
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+
+from sklearn.linear_model import LogisticRegressionCV
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+from keras.utils import np_utils
+
+
+
+
+
