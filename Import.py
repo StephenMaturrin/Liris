593fd9cd95d02e8e9b90bb58eface90e0ce9d1a7
Maturrin <migue.solinas@gmail.com>
2017-11-30 17:58:26 +0100

    1

--- Import.py
+++ Import.py
@@ -1,23 +1,29 @@
 
-from theano import function, config, shared, sandbox
-import theano.tensor as T
-import numpy
-import time
+#importing numpy and setting a seed for the computer's
+# pseudorandom number generator. This allows us to reproduce the results from our script:
+import numpy as np
+np.random.seed(123)  # for reproducibility
 
-vlen = 10 * 30 * 768  # 10 x #cores x # threads per core
-iters = 1000
+#ext, we'll import the Sequential model type from Keras. This is simply a linear stack of neural
+#  network layers, and it's perfect for the type of feed-forward CNN we're building in this tutorial.
+from keras.models import Sequential
 
-rng = numpy.random.RandomState(22)
-x = shared(numpy.asarray(rng.rand(vlen), config.floatX))
-f = function([], T.exp(x))
-print(f.maker.fgraph.toposort())
-t0 = time.time()
-for i in range(iters):
-    r = f()
-t1 = time.time()
-print("Looping %d times took %f seconds" % (iters, t1 - t0))
-print("Result is %s" % (r,))
-if numpy.any([isinstance(x.op, T.Elemwise) for x in f.maker.fgraph.toposort()]):
-    print('Used the cpu')
-else:
-    print('Used the gpu')
+#Next, let's import the "core" layers from Keras. These are the layers that are used in almost any neural network:
+from keras.layers import Dense, Dropout, Activation, Flatten
+
+#Then, we'll import the CNN layers from Keras. These are the convolutional layers that will help us efficiently train on image data:
+
+from keras.layers import Convolution2D, MaxPooling2D
+
+#we'll import some utilities. This will help us transform our data later:
+from keras.utils import np_utils
+
+#The Keras library conveniently includes it already
+from keras.datasets import mnist
+
+# Load pre-shuffled MNIST data into train and test sets
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+# (60000, 28, 28)
+print (X_train.shape)
+
---
+++ Test.py
@@ -0,0 +1,2 @@
+import  theano
+print(theano.config)
