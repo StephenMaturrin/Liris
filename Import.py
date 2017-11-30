dc88ef516aad6a856807240f734da3bd061762e0
Maturrin <migue.solinas@gmail.com>
2017-11-30 18:03:08 +0100

    1

--- Import.py
+++ Import.py
@@ -21,9 +21,13 @@ from keras.utils import np_utils
 #The Keras library conveniently includes it already
 from keras.datasets import mnist
 
+from matplotlib import pyplot as plt
 # Load pre-shuffled MNIST data into train and test sets
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
 # (60000, 28, 28)
-print (X_train.shape)
+# print (X_train.shape)
 
+
+plt.imshow(X_train[0])
+plt.show()
